fa676d8326f7ddc294aadd644299757e6293f453
TeranRyl <pachitoteranroman@gmail.com>
2023-09-25 23:19:42 -0500

    Codigo finalizado

--- src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -1,20 +1,28 @@
 package co.edu.escuelaing.sparkdockerdemolive;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static spark.Spark.*;
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class SparkWebServer {
 
-    public static void main(String... args){
+    public static void main(String... args) throws IOException {
         port(getPort());
-        get ("/sin/:numero", (request, response) -> {
+        get("/sin/:numero", (request, response) -> {
             return Math.sin(Double.parseDouble(request.params(":numero")));
-                });
-
-        get ("/cos/:numero", (request, response) -> {
+        });
+        get("/cos/:numero", (request, response) -> {
             return Math.cos(Double.parseDouble(request.params(":numero")));
         });
-        get ("/palindrome/:cadena", (request, response) -> {
+        get("/palindrome/:cadena", (request, response) -> {
             return isPalindrome(request.params(":cadena"));
+        });
+        get("/magvec/:real1/:real2", (request, response) -> {
+            return magnitudVector(request.params(":real1"), request.params(":real2"));
+        });
+        get("/index.html", (request, response) -> {
+            return getIndex();
         });
     }
 
@@ -34,6 +42,22 @@ public class SparkWebServer {
                 return false;
         }
         return true;
+    }
+
+    public static double magnitudVector(String a, String b){
+        double mag = Math.sqrt(Double.parseDouble(a) * Double.parseDouble(a) + Double.parseDouble(b) * Double.parseDouble(b));
+        return mag;
+    }
+
+    public static String getIndex() throws IOException {
+        String response;
+        Path rutaIndex = Paths.get("src/main/resources/index.html");
+        String leerIndex = Files.readString(rutaIndex);
+        return response = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                // +"Hola";*/
+                + leerIndex;
     }
 
 
